79b73e4cf0fe68a8df2bf3014a90980d1f70a28c
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-25 22:16:01 +0100

    complete implementation phase

--- EmojiPop-Storyboard/Scene.swift
+++ EmojiPop-Storyboard/Scene.swift
@@ -5,35 +5,215 @@
 //  Created by Zaid Neurothrone on 2022-12-25.
 //
 
-import SpriteKit
 import ARKit
+import SpriteKit
+
+public enum GameState {
+  case Init
+  case TapToStart
+  case Playing
+  case GameOver
+}
+
+var gameState = GameState.Init
+var anchor: ARAnchor?
+var emojis = "😁😂😛😝😋😜🤪😎🤓🤖🎃💀🤡"
+var spawnTime : TimeInterval = 0
+var score : Int = 0
+var lives : Int = 10
+
 
 class Scene: SKScene {
+  
+  func updateHUD(_ message: String) {
+    guard let sceneView = view as? ARSKView else { return }
     
-    override func didMove(to view: SKView) {
-        // Setup your scene here
+    let viewController = sceneView.delegate as! ViewController
+    viewController.hudLabel.text = message
+  }
+  
+  public func startGame() {
+    gameState = .TapToStart
+    updateHUD("- TAP TO START -")
+    
+    removeAnchor()
+  }
+  
+  public func playGame() {
+    gameState = .Playing
+    score = 0
+    lives = 10
+    spawnTime = 0
+    
+    addAnchor()
+  }
+  
+  public func stopGame() {
+    gameState = .GameOver
+    updateHUD("GAME OVER! SCORE: " + String(score))
+  }
+  
+  func addAnchor() {
+    // This casts the view as an ARSKSView so you can access the current AR session.
+    guard let sceneView = self.view as? ARSKView else { return }
+    
+    // This gets the current active frame from the AR session, which contains the camera. You’ll use the cameras transform information to create an AR anchor in front of the camera view.
+    if let currentFrame = sceneView.session.currentFrame {
+      // This calculates a new transform located 50cm in front of the camera’s view.
+      var translation = matrix_identity_float4x4
+      translation.columns.3.z = -0.5
+      let transform = simd_mul(currentFrame.camera.transform, translation)
+      
+      // Finally, this creates an AR anchor with the new transform information and adds it to the AR session.
+      anchor = ARAnchor(transform: transform)
+      sceneView.session.add(anchor: anchor!)
+    }
+  }
+  
+  func removeAnchor() {
+    guard let sceneView = self.view as? ARSKView else { return }
+    
+    if anchor != nil {
+      sceneView.session.remove(anchor: anchor!)
+    }
+  }
+  
+  func spawnEmoji() {
+    // Creates a new SKLabelNode using a random emoji character from the string of emojis available in emojis. The node is named Emoji and it’s centered vertically and horizontally.
+    let emojiNode = SKLabelNode(
+      text: String(emojis.randomElement()!)
+    )
+    
+    emojiNode.name = "Emoji"
+    emojiNode.horizontalAlignmentMode = .center
+    emojiNode.verticalAlignmentMode = .center
+    
+    // Interrogates the available node in scene, looking for the node named SpawnPoint. It then adds the newly-created emoji as a child of spawnNode. This places the emoji into the scene.
+    guard let sceneView = self.view as? ARSKView else { return }
+    
+    let spawnNode = sceneView.scene?.childNode(withName: "SpawnPoint")
+    spawnNode?.addChild(emojiNode)
+    
+    // Enable Physics
+    // This creates a new, circular-shaped physics body that you’ll attach to the emoji node’s physicsBody. The physical mass of the body is set to 10 grams.
+    emojiNode.physicsBody = SKPhysicsBody(circleOfRadius: 15)
+    emojiNode.physicsBody?.mass = 0.01
+    
+    // Add Impulse
+    emojiNode.physicsBody?.applyImpulse(
+      CGVector(
+        dx: -5 + 10 * randomCGFloat(),
+        dy: 10
+      )
+    )
+    
+    // Add Torque
+    // Positive -> Spins to the right
+    // Negative <- Spins to the left
+    emojiNode.physicsBody?.applyTorque(-0.2 + 0.4 * randomCGFloat())
+    
+    // Sound Effects & Actions
+    // Creates a few basic actions that you’ll use in just a moment. They’re fairly self-explanatory based on their names and action types.
+    let spawnSoundAction = SKAction.playSoundFileNamed(
+          "SoundEffects/Spawn.wav", waitForCompletion: false)
+    let dieSoundAction = SKAction.playSoundFileNamed(
+          "SoundEffects/Die.wav", waitForCompletion: false)
+    let waitAction = SKAction.wait(forDuration: 3)
+    let removeAction = SKAction.removeFromParent()
+    
+    // Creates a custom code block action that decreases the lives by one. When all the lives are depleted, the game stops.
+    let runAction = SKAction.run {
+      lives -= 1
+      if lives <= .zero {
+        self.stopGame()
+      }
+    }
+
+    // Creates a single action sequence that consists of all the previously-created actions. You then run the sequence action against the freshly-spawned emojis. The resulting action sequence will play out as follows, as soon as an emoji is spawned: Play spawn sound ▸ Wait for three seconds ▸ Play die sound ▸ Decrease lives / stop game ▸ Remove emojis from scene.
+    let sequenceAction = SKAction.sequence([
+      spawnSoundAction,
+      waitAction,
+      dieSoundAction,
+      runAction,
+        removeAction
+    ])
+    
+    emojiNode.run(sequenceAction)
+  }
+  
+  func randomCGFloat() -> CGFloat {
+    CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+  }
+  
+  func checkTouches(_ touches: Set<UITouch>) {
+    // This takes the first available touch from a provided list of touches. It then uses the touched screen location to do a quick raycast into the scene, determining whether the player hit any of the available SKNodes
+    guard let touch = touches.first else { return }
+    
+    let touchLocation = touch.location(in: self)
+    let touchedNode = self.atPoint(touchLocation)
+    
+    // If the player touched a node, and it’s indeed an emoji node, the score increases by 1
+    if touchedNode.name != "Emoji" { return }
+    score += 1
+    
+    // Finally, you create and run an action sequence consisting of a sound effect and an action that will remove the emoji node from its parent node — ultimately destroying the touched emoji by removing it from the scene
+    let collectSoundAction = SKAction.playSoundFileNamed(
+      "SoundEffects/Collect.wav",
+      waitForCompletion: false
+    )
+    let removeAction = SKAction.removeFromParent()
+    
+    let sequenceAction = SKAction.sequence([
+      collectSoundAction,
+      removeAction
+    ])
+    
+    touchedNode.run(sequenceAction)
+  }
+
+
+  
+  //MARK: - SKScene
+  override func didMove(to view: SKView) {
+    // Setup your scene here
+    startGame()
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
+    // Called before each frame is rendered
+    
+    // You only want to update the game while it’s in the Playing state.
+    if gameState != .Playing { return }
+    
+    // If spawnTime is 0, the game just started so you give the player a few seconds to prepare for the onslaught of emojis that are about to spawn. This creates a slight delay of 3 seconds before the first emoji spawns.
+    if spawnTime == 0 { spawnTime = currentTime + 3 }
+    
+    // Once spawnTime is less than currentTime, it’s time to spawn a new emoji. Once spawned, you reset spawnTime to wait for another half a second before spawning the next emoji.
+    if spawnTime < currentTime {
+      spawnEmoji()
+      spawnTime = currentTime + 0.5;
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    // Finally, you update the HUD with the current score and available lives.
+    updateHUD("SCORE: " + String(score) + " | LIVES: " + String(lives))
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    switch (gameState) {
+    case .Init:
+      break
+      
+    case .TapToStart:
+      playGame()
+      break
+      
+    case .Playing:
+      checkTouches(touches)
+      break
+      
+    case .GameOver:
+      startGame()
+      break
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let sceneView = self.view as? ARSKView else {
-            return
-        }
-        
-        // Create anchor using the camera's current position
-        if let currentFrame = sceneView.session.currentFrame {
-            
-            // Create a transform with a translation of 0.2 meters in front of the camera
-            var translation = matrix_identity_float4x4
-            translation.columns.3.z = -0.2
-            let transform = simd_mul(currentFrame.camera.transform, translation)
-            
-            // Add a new anchor to the session
-            let anchor = ARAnchor(transform: transform)
-            sceneView.session.add(anchor: anchor)
-        }
-    }
+  }
 }
--- EmojiPop-Storyboard/ViewController.swift
+++ EmojiPop-Storyboard/ViewController.swift
@@ -9,65 +9,157 @@ import UIKit
 import SpriteKit
 import ARKit
 
-class ViewController: UIViewController, ARSKViewDelegate {
+class ViewController: UIViewController {
+  
+  @IBOutlet var sceneView: ARSKView!
+  @IBOutlet weak var hudLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet var sceneView: ARSKView!
+    // Set the view's delegate
+    sceneView.delegate = self
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and node count
-        sceneView.showsFPS = true
-        sceneView.showsNodeCount = true
-        
-        // Load the SKScene from 'Scene.sks'
-        if let scene = SKScene(fileNamed: "Scene") {
-            sceneView.presentScene(scene)
-        }
+    // Show statistics such as fps and node count
+    sceneView.showsFPS = true
+    sceneView.showsNodeCount = true
+    
+    // Load the SKScene from 'Scene.sks'
+    if let scene = SKScene(fileNamed: "Scene") {
+      sceneView.presentScene(scene)
     }
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
+    // Create a session configuration
+    let configuration = ARWorldTrackingConfiguration()
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
+    // Run the view's session
+    sceneView.session.run(configuration)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
     
-    // MARK: - ARSKViewDelegate
+    // Pause the view's session
+    sceneView.session.pause()
+  }
+  
+  //MARK: - Custom methods
+  func showAlert(_ title: String, _ message: String) {
+    let alert = UIAlertController(
+      title: title,
+      message: message,
+      preferredStyle: .alert
+    )
     
-    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
-        // Create and configure a node for the anchor added to the view's session.
-        let labelNode = SKLabelNode(text: "👾")
-        labelNode.horizontalAlignmentMode = .center
-        labelNode.verticalAlignmentMode = .center
-        return labelNode;
-    }
+    alert.addAction(
+      UIAlertAction(
+        title: "OK",
+        style: UIAlertAction.Style.default,
+        handler: nil
+      )
+    )
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+    self.present(alert, animated: true, completion: nil)
+  }
 }
+
+// MARK: - ARSKViewDelegate conformance
+extension ViewController: ARSKViewDelegate {
+  func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+    // After the app creates the AR anchor, you’ll use the delegate to provide a SKNode for the new anchor. This SpriteKit node acts as the Spawn Point for the game.
+    
+    // This creates an empty SpriteKit node and sets its name to SpawnPoint
+    let spawnNode = SKNode()
+    spawnNode.name = "SpawnPoint"
+    
+    // To give the player a visual indicator of where the spawn point is in the real world, this creates a little SOS box and adds it as a child of the spawn point node
+    let boxNode = SKLabelNode(text: "🆘")
+    boxNode.verticalAlignmentMode = .center
+    boxNode.horizontalAlignmentMode = .center
+    boxNode.zPosition = 100
+    boxNode.setScale(0)
+    spawnNode.addChild(boxNode)
+    
+    // Animate SOS up scale: This creates and runs an action sequence consisting of a sound effect and a scale effect on the created box node. It slowly scales the box to 1.5 while playing a nice sound effect.
+    let startSoundAction = SKAction.playSoundFileNamed(
+      "SoundEffects/GameStart.wav",
+      waitForCompletion: false
+    )
+    let scaleInAction = SKAction.scale(to: 1.5, duration: 0.8)
+    
+    boxNode.run(
+      SKAction.sequence([
+        startSoundAction,
+        scaleInAction
+      ])
+    )
+    
+    // Finally, the spawnNode is provided as the SKNode for the newly-added AR anchor. This also links the spawn node to the AR anchor. Any changes to the AR anchor will be synced to the spawn node.
+    return spawnNode
+
+    
+//    // Create and configure a node for the anchor added to the view's session.
+//    let labelNode = SKLabelNode(text: "👾")
+//    labelNode.horizontalAlignmentMode = .center
+//    labelNode.verticalAlignmentMode = .center
+//    return labelNode;
+  }
+  
+  //MARK: - Handling AR Session Failures: Typically occur when the AR session has stopped due to some kind of failure
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    showAlert("Session Failure", error.localizedDescription)
+  }
+  
+  //MARK: - Handling AR Session Interruptions: This issue happens when the session has temporarily stopped processing frames and device position tracking — typically because the player took a phone call or switched to a different app
+  func sessionWasInterrupted(_ session: ARSession) {
+    // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    showAlert("AR Session", "Session was interrupted!")
+  }
+  
+  func sessionInterruptionEnded(_ session: ARSession) {
+    // Reset tracking and/or remove existing anchors if consistent tracking is required
+    let scene = sceneView.scene as! Scene
+    scene.startGame()
+  }
+  
+  //MARK: - Handling Camera Tracking Issues: These occur when the quality of ARKit’s position tracking has degraded for some reason
+  func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+    // You can access the current tracking state through the provided camera. Th switch statement then handles all possible cases. If there’s a problem, you then notify the player with an alert message.
+    switch camera.trackingState {
+    case .normal: break
+    case .notAvailable:
+      showAlert("Tracking Limited", "AR not available")
+      break
+      // When tracking is limited, you can dig deeper to find out exactly why. Again, you have a few cases to deal with. You’ll then send the player an alert message with the result.
+    case .limited(let reason):
+      switch reason {
+      case .initializing, .relocalizing: break
+      case .excessiveMotion:
+        showAlert("Tracking Limited", "Excessive motion!")
+        break
+      case .insufficientFeatures:
+        showAlert("Tracking Limited", "Insufficient features!")
+        break
+      default: break
+      }
+    }
+  }
+}
+
+/*
+ ARSKViewDelegate functions:
+ 
+ func view(_:nodeFor:) -> SKNode: Call this when the app adds a new AR anchor. Note that it returns a SKNode, so this is a good place to create and link a SpriteKit node to the newly-added AR anchor.
+
+ func view(_:didAdd:for:): Informs the delegate that a SpriteKit node related to a new AR anchor has been added to the scene.
+
+ func view(_:willUpdate:for:): Informs the delegate that a SpriteKit node will be updated based on changes to the related AR anchor.
+
+ func view(_:didUpdate:for:): Informs the delegate that a SpriteKit node has been updated to match changes on the related AR anchor.
+
+ func view(_:didRemove:for:): Informs the delegate that the SpriteKit node has been removed from the scene on the related AR anchor.
+ */
